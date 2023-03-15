37882a5f62835eb4e99a60884fca4da78b5ebb00
MachireddyManasa <machireddymanasa03@gmail.com>
2023-03-15 14:55:14 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def jadeja(request):
+    return render(request,'jadeja.html')
+
+
+
+
+def rohit(request):
+    return render(request,'rohit.html')
